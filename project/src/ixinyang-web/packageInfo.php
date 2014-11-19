ea1cc1fbf063d40af4e75ddab4aed48f97a31f5e
hywxu <xuwei@xuwei-2.local>
2014-11-20 02:15:28 +0800

    创建项目
    项目初始化

---
+++ project/src/ixinyang-web/packageInfo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 项目开发范围
+ * 		1、爱信阳项目web端代码
+ * 		2、爱信阳项目server端代码
+ * 
+ * 项目开发技术选型
+ * 		1、开发语言： php
+ * 		2、技术框架： yii2框架
+ * 		3、数据库选型： mysql
+ *
+ * 项目开发计划： 详细见worktile
+ *
+ * php规范： 详细见worktile
+ * 
+ * php注释规范：
+ * 文件头注释
+ *  /**
+ * 	 * @link http://www.huayancreate.com/
+ *   * @copyright Copyright (c) 2014 HuaYan
+ *   * @license http://www.huayancreate.com/license/
+ *	*/
+/**
+ *  class注释
+ *  /**
+ *   * @author xuwei <wxu@huayancreate.com>
+ *   * @version 1.0
+ *	*/
+ 
+
+?>
