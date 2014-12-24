53a1d3b186999bfe0af1ccacf31d388d8045f249
hymawang <1284820482@qq.com>
2014-12-24 17:59:35 +0800

    autoload文件修改
    autoload文件修改

--- project/src/ixinyang-web/ixinyang/common/config/autoload.php
+++ project/src/ixinyang-web/ixinyang/common/config/autoload.php
@@ -1,5 +1,3 @@
 <?php
 require_once(__DIR__ . '/../hycommon/widgets/AreaDecorator.php');
 require_once(__DIR__ . '/constant.php');
-require_once(__DIR__ . '/../../vendor/wisembly/elephant.io/src/Client.php');
-require_once(__DIR__ . '/../../vendor/wisembly/elephant.io/src/Engine/SocketIO/Version1X.php');
