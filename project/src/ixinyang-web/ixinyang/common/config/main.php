8087ecf74a6f9484fe0f77d697912bdced16665a
huayanzp <pzhu@huayancreate.com>
2015-01-22 10:32:09 +0800

    修正

--- project/src/ixinyang-web/ixinyang/common/config/main.php
+++ project/src/ixinyang-web/ixinyang/common/config/main.php
@@ -5,16 +5,17 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'language' => 'zh-CN',
+    'bootstrap' => ['log'],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-            'log' => [
-                'targets' => [
-                    'file' => [
-                        'class' => 'yii\log\FileTarget',
-                        'levels' => [ 'error'],
-                        'categories' => ['yii\*'],
-                    ],
+        ],
+        'log' => [
+            'targets' => [
+                'file' => [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => [ 'error'],
+                    'categories' => ['yii\*'],
                 ],
             ],
         ],
